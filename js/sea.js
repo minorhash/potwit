7e0b79ef7f3adb08aaad0383a44b4d9fe1bc0977
minorhash <minorhash@gmail.com>
2018-09-04 15:07:19 +0900

    09/04/18

--- js/sea.js
+++ js/sea.js
@@ -1,25 +1,15 @@
 var Twit = require('twit')
-var cnf=require("./cnf.json")
+var cnf=require("../cnf.json")
 
 var T = new Twit({
   consumer_key:        cnf.key,
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
+  consumer_secret:     cnf.sec,
+  access_token:        cnf.tok,
+  access_token_secret: cnf.tsc,
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
 
-//
-//  tweet 'hello world!'
-//
-T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-  console.log(data)
-})
-
-//
-//  search twitter for all tweets containing the word 'banana' since July 11, 2011
-//
-T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
-  console.log(data)
+T.get('search/tweets', { q: 'tamale:2011-07-11', count: 100 }, function(err, data, res) {
+  console.log(res)
 })
