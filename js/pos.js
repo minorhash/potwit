279cb6deb94ffa7665b96f3ae40b5e0c9f602650
minorhash <minorhash@gmail.com>
2019-03-29 19:55:39 +0900

    03/29/19

--- js/pos.js
+++ js/pos.js
@@ -10,21 +10,7 @@ var T = new Twit({
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
 
-var que="白石冬美 訃報"
-
-T.get('search/tweets', { q: que, count: 3}, function(err, dat, res) {
-
-  //console.log(dat)
-    var arr=dat.statuses
-console.log(arr.length)
-
-for(var i=0;i<arr.length;i++){
-//console.log(i)
-//console.log(arr[i].lang)
-if(arr[i].lang=="ja"){
-console.log(arr[i].id)
-
-}
-}
-
+var key="あにきー！"
+T.post('statuses/update', { status: key }, function(err, data, response) {
+  console.log(data)
 })
